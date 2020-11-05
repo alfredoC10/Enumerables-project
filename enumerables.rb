0073a8aaa034df3cb02ec3d60e45d7fb4c850732
Huemac-Alfredo <hmca101292@outlook.com>
2020-11-05 12:58:49 -0600

    Build my_any method

--- enumerables.rb
+++ enumerables.rb
@@ -24,4 +24,173 @@ module Enumerable
 
 
 #3.- my_select Method 
+  def my_select
+    return to_enum(:my_select) unless block_given?
+    if self.is_a? Hash
+      new_hsh = {}
+      self.my_each do |key, value| 
+        if yield(key, value)
+          new_hsh[key] = value
+        end
+      end
+      new_hsh
+    elsif self.is_a? String
+      return "Wrong input"
+    else
+      new_arr = []
+      self.my_each do |elmt|
+        if yield(elmt)
+          filtered_arr.push(elmt)
+        end
+      end
+      return new_arr
+    end
+  end
+
+#4.- my_all? method
+  def my_all?(*arg)
+    counter = 0
+
+    if block_given?
+      self.my_each do |elmt| 
+        if yield(elmt) == true
+          counter += 1 
+        end
+      end
+      if counter == self.to_a.length 
+        true
+      else
+        false
+      end
+
+    elsif arg[0].instance_of? Class 
+      self.my_each do |elmt|
+        if elmt.class == arg[0]
+          counter += 1
+        end
+      end
+      if counter == self.to_a.length 
+        true
+      else
+        false
+      end
+
+    elsif arg[0].is_a? Object
+
+      if arg[0].class == Regexp
+        self.my_each do |elmt|
+          if elmt.class != String
+            false
+          elsif elmt.match?(arg[0])
+            counter += 1
+          end
+        end
+        if counter == self.to_a.length
+          true
+        else
+          false
+        end
+
+      elsif arg.size == 0
+        self.my_each do |elmt|
+          if elmt != false && elmt != nil
+            counter +=1
+          end
+        end
+        if counter == self.to_a.length
+          true
+        else
+          false
+        end
+
+      else
+        self.my_each do |elmt|
+          if elmt == arg[0]
+            counter += 1
+          end
+        end
+        if counter == self.to_a.length 
+          true
+        else
+          false
+        end
+      end
+
+    end
+
+  end
+
+#5.- my_any? method
+  def my_any?(*arg)
+    counter = 0
+  # Was a block given to the method?
+    if block_given?
+      self.my_each do |elmt| 
+        if yield(elmt) == true
+          counter += 1 
+        end
+      end
+      if counter > 0 
+        true
+      else
+        false
+      end
+    # Is the argument an Integer, Float, string, etc.?
+    elsif arg[0].instance_of? Class 
+      self.my_each do |elmt|
+        if elmt.class == arg[0]
+          counter += 1
+        end
+      end
+      if counter > 0 
+        true
+      else
+        false
+      end
+    # When my_any is used to find if there's a specific object within the collection 
+    elsif arg[0].is_a? Object
+
+      if arg[0].class == Regexp #Does any of the elements match the regular expression?
+        self.my_each do |elmt|
+          if elmt.class != String
+            false
+          elsif elmt.match?(arg[0])
+            counter += 1
+          end
+        end
+        if counter > 0
+          true
+        else
+          false
+        end
+      #When my_any is used without a block nor an argument
+      elsif arg.size == 0
+        self.my_each do |elmt|
+          if elmt != false && elmt != nil
+            counter +=1
+          end
+        end
+        if counter > 0
+          true
+        else
+          false
+        end
+
+      else
+        self.my_each do |elmt|
+          if elmt == arg[0]
+            counter += 1
+          end
+        end
+        if counter > 0 
+          true
+        else
+          false
+        end
+      end
+
+    end
+
+  end
+
 end
