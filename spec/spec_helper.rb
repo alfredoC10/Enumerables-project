2debc5440bcae2e7648502ac5e795903884748bc
Huemac Alfredo Córdova <hmca101292@outlook.com>
2020-12-11 00:42:22 -0600

    Fix linter errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,7 +44,7 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-# The settings below are suggested to provide a good initial experience
+  # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
@@ -76,7 +76,7 @@ RSpec.configure do |config|
   # Use the documentation formatter for detailed output,
   # unless a formatter has already been configured
   # (e.g. via a command-line flag).
-  #config.default_formatter = "doc"
+  # config.default_formatter = "doc"
   # end
 
   # Print the 10 slowest examples and example groups at the
@@ -94,5 +94,5 @@ RSpec.configure do |config|
   # Setting this allows you to use `--seed` to deterministically reproduce
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
-  #Kernel.srand config.seed
+  # Kernel.srand config.seed
 end
