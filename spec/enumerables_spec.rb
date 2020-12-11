d8635d87ffc181e98da70086597845d7aba50b12
Huemac Alfredo Córdova <hmca101292@outlook.com>
2020-12-11 09:55:53 -0600

    Fix rubocop offenses

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,4 +1,4 @@
-require './enumerables.rb'
+require './enumerables'
 
 describe Enumerable do
   let(:arr_str) { %w[lion ant bear cat fish] }
@@ -10,7 +10,7 @@ describe Enumerable do
 
   describe '#my_each' do
     it 'Returns the same input array' do
-      expect(arr_str.my_each { |i| i + 'hi' }).to eql(arr_str)
+      expect(arr_str.my_each { |i| "#{i}" + "hi" }).to eql(arr_str)
     end
 
     it 'Creates an enumererator if no block is given' do
@@ -22,7 +22,7 @@ describe Enumerable do
       arr_int.my_each { |x| nw.push(x + 2) }
       expect(nw).to eql([3, 4, 5, 6, 7])
       empt = []
-      arr_str.my_each { |i| empt.push(i + '-hi') }
+      arr_str.my_each { |i| empt.push("#{i}" + "-hi") }
       expect(empt).to eql(%w[lion-hi ant-hi bear-hi cat-hi fish-hi])
     end
   end
